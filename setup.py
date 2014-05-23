4649f58004928a6f7c20a1d72e817466a6f81514
twneale <twneale@gmail.com>
2014-05-22 22:35:53 -0400

    Bersion bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from nmmd import VERSION
+
 
 long_description = '''
 Tools abound for single/double/multiple dispatch in python, but most
@@ -9,7 +9,7 @@ injecting names into lower stack frames, allowing strings to be
 passed as code and executed in the dispatchers context, and
 other things this package deems silly and/or garbage.
 '''
-VERSION = (0, 0, 2)
+VERSION = (0, 0, 3)
 __version__ = '.'.join(str(p) for p in VERSION[0:3]) + ''.join(VERSION[3:])
 
 setup(name='nmmd',
